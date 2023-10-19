6512d2c30e8ded7768be8fe0be59fb40a0ca34dd
I'm DaK0Ta <arsbroart@mail.ru>
2023-10-19 20:43:49 +0300

    feat: add guard clauses

--- src/ArturKuznetsov/lab2/task3/PrimeChecker.java
+++ src/ArturKuznetsov/lab2/task3/PrimeChecker.java
@@ -4,15 +4,16 @@ import static java.lang.Math.sqrt;
 
 public class PrimeChecker {
     public boolean IsPrime(long num) {
-        if (num > 1) {
-            for (long i = 2; i <= sqrt(num); i++) {
-                if (num % i == 0) {
-                    return false;
-                }
-            }
-            return true;
-        } else {
+        if (num <= 1) {
             return false;
         }
+
+        for (long i = 2; i <= sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
