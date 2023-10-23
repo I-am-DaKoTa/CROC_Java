1b70cd9fe0e8fce54201e8fb30490f5dec6db727
I'm DaK0Ta <arsbroart@mail.ru>
2023-10-24 00:25:51 +0300

    feat: implement functionality for task 5

---
+++ src/ArturKuznetsov/lab3/task5/Main.java
@@ -0,0 +1,35 @@
+package ArturKuznetsov.lab3.task5;
+
+import ArturKuznetsov.lab3.task5.builders.FridgeBuilder;
+import ArturKuznetsov.lab3.task5.builders.RobotVacuumCleanerBuilder;
+import ArturKuznetsov.lab3.task5.builders.WashingMachineBuilder;
+import ArturKuznetsov.lab3.task5.director.Director;
+import ArturKuznetsov.lab3.task5.exceptions.InvalidValueException;
+import ArturKuznetsov.lab3.task5.exceptions.NotImportedProductException;
+
+public class Main {
+    public static void main(String[] args) throws NotImportedProductException, InvalidValueException {
+        Director director = new Director();
+
+        var fridgeBuilder = new FridgeBuilder();
+        var robotVacuumCleanerBuilder = new RobotVacuumCleanerBuilder();
+        var washingMachineBuilder = new WashingMachineBuilder();
+
+        director.constructHyundaiFridge(fridgeBuilder);
+        director.constructRedmondRobotVacuumCleaner(robotVacuumCleanerBuilder);
+        director.constructSamsungWashingMachine(washingMachineBuilder);
+
+        var fridge = fridgeBuilder.build();
+        var robotVacuumCleaner = robotVacuumCleanerBuilder.build();
+        var washingMachine = washingMachineBuilder.build();
+
+        var catalog = new Catalog();
+        catalog.addProduct(fridge);
+        catalog.addProduct(robotVacuumCleaner);
+        catalog.addProduct(washingMachine);
+
+        catalog.showAllCatalog();
+
+        System.out.println(fridge.getImportProductDetails());
+    }
+}
