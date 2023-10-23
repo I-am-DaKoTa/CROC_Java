71b7ec884c14c5ec7f3f1cdd304770663de58e63
I'm DaK0Ta <arsbroart@mail.ru>
2023-10-24 00:07:27 +0300

    feat: create NotImportedProductException class for handling exceptions for a non-imported product

---
+++ src/ArturKuznetsov/lab3/task5/exceptions/NotImportedProductException.java
@@ -0,0 +1,7 @@
+package ArturKuznetsov.lab3.task5.exceptions;
+
+public class NotImportedProductException extends Exception {
+    public NotImportedProductException(String message) {
+        super(message);
+    }
+}
