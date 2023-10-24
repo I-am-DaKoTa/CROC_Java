52052767da6fe864828bfd71eafaf2946f6c4e2a
I'm DaK0Ta <arsbroart@mail.ru>
2023-10-24 09:50:44 +0300

    feat: add Annotation NotFoundException class for undiscovered annotations

---
+++ src/ArturKuznetsov/lab3/task6/exceptions/AnnotationNotFoundException.java
@@ -0,0 +1,7 @@
+package ArturKuznetsov.lab3.task6.exceptions;
+
+public class AnnotationNotFoundException extends Exception {
+    public AnnotationNotFoundException(String message) {
+        super(message);
+    }
+}
