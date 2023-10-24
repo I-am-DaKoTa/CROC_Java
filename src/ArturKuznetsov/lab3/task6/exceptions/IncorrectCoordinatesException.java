8579126530a9f6424ca25eb4b9438a5a5d184075
I'm DaK0Ta <arsbroart@mail.ru>
2023-10-24 09:48:18 +0300

    feat: add IncorrectCoordinatesException class for incorrect coordinates

---
+++ src/ArturKuznetsov/lab3/task6/exceptions/IncorrectCoordinatesException.java
@@ -0,0 +1,7 @@
+package ArturKuznetsov.lab3.task6.exceptions;
+
+public class IncorrectCoordinatesException extends Exception {
+    public IncorrectCoordinatesException(String message) {
+        super(message);
+    }
+}
