e4fd957cae2556e7b63b6196eee02e0c017ac3be
I'm DaK0Ta <arsbroart@mail.ru>
2023-10-28 19:52:02 +0300

    Update Catalog.java

--- src/ArturKuznetsov/lab3/task5/Catalog.java
+++ src/ArturKuznetsov/lab3/task5/Catalog.java
@@ -1,5 +1,6 @@
 package ArturKuznetsov.lab3.task5;
 
+import ArturKuznetsov.lab3.task5.exceptions.NotImportedProductException;
 import ArturKuznetsov.lab3.task5.products.Product;
 
 import java.util.ArrayList;
@@ -16,5 +17,20 @@ public class Catalog {
         for (Product product : products) {
             System.out.println(product.displayProductInfo());
         }
+    }
+
+    public void showFirstImportedThenLocal() {
+        StringBuilder importedProducts = new StringBuilder();
+        StringBuilder localProducts = new StringBuilder();
+        for (Product product : products) {
+            try {
+                product.isImportedException();
+                importedProducts.append(product.displayProductInfo()).append("\n");
+            } catch (NotImportedProductException e) {
+                localProducts.append(product.displayProductInfo()).append("\n");
+            }
+        }
+        System.out.println(importedProducts);
+        System.out.println(localProducts);
     }
 }
