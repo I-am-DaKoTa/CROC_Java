aaa20e7414dd447d3a6e865e8ce318c094d74b04
I'm DaK0Ta <arsbroart@mail.ru>
2023-10-28 19:51:58 +0300

    Update Main.java

--- src/ArturKuznetsov/lab3/task5/Main.java
+++ src/ArturKuznetsov/lab3/task5/Main.java
@@ -30,6 +30,15 @@ public class Main {
 
         catalog.showAllCatalog();
 
+        System.out.println("================================================\n");
+
+        catalog.showFirstImportedThenLocal();
+
+        System.out.println("================================================\n");
+
         System.out.println(fridge.getImportProductDetails());
+        System.out.println(robotVacuumCleaner.getImportProductDetails());
+        System.out.println(washingMachine.getImportProductDetails());
+
     }
 }
