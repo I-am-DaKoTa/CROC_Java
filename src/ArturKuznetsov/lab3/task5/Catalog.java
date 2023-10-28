45a9f5cb9bbf923f45b0b180a71744d8f7ec5fe7
I'm DaK0Ta <arsbroart@mail.ru>
2023-10-28 22:24:36 +0300

    Revert "Update Catalog.java"
    This reverts commit e4fd957cae2556e7b63b6196eee02e0c017ac3be.

--- src/ArturKuznetsov/lab3/task5/Catalog.java
+++ src/ArturKuznetsov/lab3/task5/Catalog.java
@@ -1,6 +1,5 @@
 package ArturKuznetsov.lab3.task5;
 
-import ArturKuznetsov.lab3.task5.exceptions.NotImportedProductException;
 import ArturKuznetsov.lab3.task5.products.Product;
 
 import java.util.ArrayList;
@@ -17,20 +16,5 @@ public class Catalog {
         for (Product product : products) {
             System.out.println(product.displayProductInfo());
         }
-    }
-
-    public void showFirstImportedThenLocal() {
-        StringBuilder importedProducts = new StringBuilder();
-        StringBuilder localProducts = new StringBuilder();
-        for (Product product : products) {
-            try {
-                product.isImportedException();
-                importedProducts.append(product.displayProductInfo()).append("\n");
-            } catch (NotImportedProductException e) {
-                localProducts.append(product.displayProductInfo()).append("\n");
-            }
-        }
-        System.out.println(importedProducts);
-        System.out.println(localProducts);
     }
 }
