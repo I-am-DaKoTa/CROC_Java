6230db03979c27d3ac6165b987150834284e68b2
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-01 16:17:44 +0300

    Create InvalidCustomerData.java

---
+++ src/ArturKuznetsov/lab4/task7/exceptions/InvalidCustomerData.java
@@ -0,0 +1,7 @@
+package ArturKuznetsov.lab4.task7.exceptions;
+
+public class InvalidCustomerData extends Exception {
+    public InvalidCustomerData(String message) {
+        super(message);
+    }
+}
