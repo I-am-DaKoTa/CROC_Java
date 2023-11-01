cfbf80a69c5fd0cc92e69432e3d60aca77598e3d
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-01 16:18:01 +0300

    Create OrderStatusExpection.java

---
+++ src/ArturKuznetsov/lab4/task7/exceptions/OrderStatusExpection.java
@@ -0,0 +1,7 @@
+package ArturKuznetsov.lab4.task7.exceptions;
+
+public class OrderStatusExpection extends Exception {
+    public OrderStatusExpection(String message) {
+        super(message);
+    }
+}
