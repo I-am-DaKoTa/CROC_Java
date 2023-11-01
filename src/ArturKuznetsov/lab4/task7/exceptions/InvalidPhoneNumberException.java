5c7bf74afc9b0b981b3eabca0e18a8b57b3c2a14
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-01 16:17:58 +0300

    Create InvalidPhoneNumberException.java

---
+++ src/ArturKuznetsov/lab4/task7/exceptions/InvalidPhoneNumberException.java
@@ -0,0 +1,7 @@
+package ArturKuznetsov.lab4.task7.exceptions;
+
+public class InvalidPhoneNumberException extends Exception {
+    public InvalidPhoneNumberException(String message) {
+        super(message);
+    }
+}
