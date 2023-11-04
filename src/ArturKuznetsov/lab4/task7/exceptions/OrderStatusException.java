adbb6c56279fe5b360dd9095fe133ff9f67de123
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-05 02:13:13 +0300

    refactor(OrderStatusException): change class name

---
+++ src/ArturKuznetsov/lab4/task7/exceptions/OrderStatusException.java
@@ -0,0 +1,7 @@
+package ArturKuznetsov.lab4.task7.exceptions;
+
+public class OrderStatusException extends Exception {
+    public OrderStatusException(String message) {
+        super(message);
+    }
+}
--- src/ArturKuznetsov/lab4/task7/exceptions/OrderStatusExpection.java
+++
@@ -1,7 +0,0 @@
-package ArturKuznetsov.lab4.task7.exceptions;
-
-public class OrderStatusExpection extends Exception {
-    public OrderStatusExpection(String message) {
-        super(message);
-    }
-}
