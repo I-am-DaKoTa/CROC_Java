1c1b5a0b9981e38ae818fd2c7f89359712e1da5b
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-05 02:18:35 +0300

    refactor(Notification): implement interface and add a null check

--- src/ArturKuznetsov/lab4/task7/Notification.java
+++ src/ArturKuznetsov/lab4/task7/Notification.java
@@ -1,11 +1,15 @@
 package ArturKuznetsov.lab4.task7;
 
 import ArturKuznetsov.lab3.task5.products.Product;
+import ArturKuznetsov.lab4.task7.enums.Gender;
+import ArturKuznetsov.lab4.task7.exceptions.OrderStatusException;
+import ArturKuznetsov.lab4.task7.interfaces.INotification;
 
+import java.text.NumberFormat;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
+import java.util.Locale;
 
-public class Notification {
+public class Notification implements INotification {
     Order order;
     Client client;
 
@@ -16,7 +20,7 @@ public class Notification {
 
     private String getGreeting() {
         String ending = "ый";
-        if (Objects.equals(client.getGender(), "Женщина")){
+        if (client.getGender() == Gender.FEMALE){
             ending = "ая";
         }
         return String.format("Уважаем%s %s!\nРады сообщить, что Ваш заказ %s готов к выдаче.\n",ending, client.getFullName(), order.getId());
@@ -29,16 +33,25 @@ public class Notification {
             orderListInfo.append(product.displayProductInfo()).append('\n');
             totalPrice += product.getPrice();
         }
-        orderListInfo.append(String.format("Сумма к оплате: %.2f₽\n", totalPrice));
+        Locale russian = new Locale("ru", "RU");
+        NumberFormat russianFormat = NumberFormat.getCurrencyInstance(russian);
+        orderListInfo.append(String.format("Сумма к оплате: %s\n", russianFormat.format(totalPrice)));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         orderListInfo.append(String.format("Срок хранения заказа %s.\n",order.getCollectedDateTime().plusWeeks(2).format(formatter)));
         return orderListInfo.toString();
     }
     @Override
     public String toString() {
-        String notification = getGreeting();
-        notification += getOrderListInfo();
-        notification += "С наилучшими пожеланиями, магазин \"Кошки и картошки\"";
+        String notification = "Заказ ещё не собран";
+        try {
+            if (order.canGetOrder()) {
+                notification = getGreeting();
+                notification += getOrderListInfo();
+                notification += "С наилучшими пожеланиями, магазин \"Кошки и картошки\"";
+            }
+        } catch (OrderStatusException e) {
+            return notification;
+        }
         return notification;
     }
 }
