13b4776f978606ee629f94e51544e5b6ee094da4
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-06 21:45:46 +0300

    feat: add IBlackListFilter interface

---
+++ src/ArturKuznetsov/lab5/task9/IBlackListFilter.java
@@ -0,0 +1,20 @@
+package ArturKuznetsov.lab5.task9;
+
+import java.util.List;
+import java.util.Set;
+
+public interface IBlackListFilter {
+
+    /**
+     * From the given list of comments removes ones
+     * that contain words from the black list.
+     *
+     * @param comments list of comments; every comment
+     *                 is a sequence of words, separated
+     *                 by spaces, punctuation or line breaks
+     * @param blackList list of words that should not
+     *                  be present in a comment
+     */
+    void filterComments(List<String> comments, Set<String> blackList);
+
+}
