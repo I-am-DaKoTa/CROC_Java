b97904869f71a620047c829d99bac3a2bd06a688
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-15 01:26:09 +0300

    feat(InvalidTextException): change extends to RuntimeException

--- src/ArturKuznetsov/lab5/task9/InvalidTextException.java
+++ src/ArturKuznetsov/lab5/task9/InvalidTextException.java
@@ -1,6 +1,6 @@
 package ArturKuznetsov.lab5.task9;
 
-public class InvalidTextException extends Exception {
+public class InvalidTextException extends RuntimeException {
     public InvalidTextException(String message) {
         super(message);
     }
