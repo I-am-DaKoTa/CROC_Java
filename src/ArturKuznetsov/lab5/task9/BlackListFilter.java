de45e3b1f48305c0f98db758879c5ae23f4d6b8e
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-15 01:29:47 +0300

    feat(BlackListFilter): update the logic of checking comments
    - add the initial comments and blacklist check
    - adda check on which words from the blackList are contained in the comment and in the future I use the newBlackList to replace words

--- src/ArturKuznetsov/lab5/task9/BlackListFilter.java
+++ src/ArturKuznetsov/lab5/task9/BlackListFilter.java
@@ -1,6 +1,7 @@
 package ArturKuznetsov.lab5.task9;
 
-import java.util.Arrays;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -9,54 +10,78 @@ import java.util.regex.Pattern;
 public class BlackListFilter implements IBlackListFilter {
     @Override
     public void filterComments(List<String> comments, Set<String> blackList) {
-        String regex = "[.,!?:;\\-\"'()\\s]+";
-        Pattern pattern = Pattern.compile(regex);
+        if (comments == null ||  comments.isEmpty()) {
+            throw new InvalidTextException("Список комментариев не может быть пустым или равным null");
+        }
+        if ( blackList == null || blackList.isEmpty()) {
+            throw new InvalidTextException("Чёрный список не может быть пустым или равным null");
+        }
+
         int commentIndex = 0;
         while (commentIndex < comments.size()) {
-            try {
-                if (comments.get(commentIndex) == null) {
-                    throw new InvalidTextException("Сообщение не может быть равным null");
-                }
-            } catch (InvalidTextException e) {
-                System.out.println(e.getMessage());
+            String comment = comments.get(commentIndex);
+
+            if (comment == null) {
                 comments.remove(commentIndex);
                 continue;
             }
 
-            String comment = comments.get(commentIndex);
-            StringBuilder verifiedComment = new StringBuilder();
-
-            Matcher matcher = pattern.matcher(comment);
-            int start = 0;
-            while (matcher.find()) {
-                int end = matcher.start();
-                if (end > start) {
-                    String wordFromComment = comment.substring(start, end);
-                    if (!containsWordFromBlackList(blackList, wordFromComment, verifiedComment)) {
-                        verifiedComment.append(wordFromComment);
-                    }
+            Set<String> newBlackList = new HashSet<>();
+            for (String wordFromBlackList : blackList) {
+                if (commentContainsWordFromBlackList(wordFromBlackList, comment)) {
+                    newBlackList.add(wordFromBlackList);
                 }
-                String match = matcher.group();
-                verifiedComment.append(match);
-                start = matcher.end();
             }
 
-            if (start < comment.length()) {
-                String wordFromComment = comment.substring(start);
-                verifiedComment.append(wordFromComment);
+            if (newBlackList.isEmpty()) {
+                commentIndex++;
+                continue;
             }
 
-            comments.set(commentIndex, String.valueOf(verifiedComment));
+            comments.set(commentIndex, String.valueOf(checkWordsInComment(newBlackList, comment)));
             commentIndex++;
         }
     }
-    private String replaceWithStars(String word) {
-        char[] stars = new char[word.length()];
-        Arrays.fill(stars, '*');
-        return new String(stars);
+
+    private boolean commentContainsWordFromBlackList(String wordFromBlackList, String comment) {
+        String regex = "\\b" + wordFromBlackList + "\\b";
+        Matcher matcher = Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(comment);
+
+        return matcher.find();
     }
 
-    private boolean containsWordFromBlackList(Set<String> blackList, String wordFromComment, StringBuilder verifiedComment) {
+    private StringBuilder checkWordsInComment(Set<String> newBlackList, String comment) {
+        String regex = "[.,!?:;\\-\"'()\\s]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(comment);
+
+        StringBuilder replacedComment = new StringBuilder();
+
+        int start = 0;
+        while (matcher.find()) {
+            int end = matcher.start();
+            if (end > start) {
+                String wordFromComment = comment.substring(start, end);
+
+                if (!isWordFromBlackList(newBlackList, wordFromComment, replacedComment)) {
+                    replacedComment.append(wordFromComment);
+                }
+
+            }
+            String match = matcher.group();
+            replacedComment.append(match);
+            start = matcher.end();
+        }
+
+        if (start < comment.length()) {
+            String wordFromComment = comment.substring(start);
+            replacedComment.append(wordFromComment);
+        }
+
+        return replacedComment;
+    }
+
+    private boolean isWordFromBlackList(Set<String> blackList, String wordFromComment, StringBuilder verifiedComment) {
         for (String word : blackList) {
             if (wordFromComment.equalsIgnoreCase(word)) {
                 verifiedComment.append(replaceWithStars(wordFromComment));
@@ -64,5 +89,9 @@ public class BlackListFilter implements IBlackListFilter {
             }
         }
         return false;
+    }
+
+    private String replaceWithStars(String word) {
+        return "*".repeat(word.length());
     }
 }
