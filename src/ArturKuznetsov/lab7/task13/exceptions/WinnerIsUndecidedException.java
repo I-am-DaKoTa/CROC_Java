e58f90a5577bd3daa00c43ce90f35bb46e89100b
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-21 00:40:18 +0300

    feat(WinnerIsUndecidedException): create exception when winner is undecided

---
+++ src/ArturKuznetsov/lab7/task13/exceptions/WinnerIsUndecidedException.java
@@ -0,0 +1,7 @@
+package ArturKuznetsov.lab7.task13.exceptions;
+
+public class WinnerIsUndecidedException extends RuntimeException {
+    public WinnerIsUndecidedException(String message) {
+        super(message);
+    }
+}
