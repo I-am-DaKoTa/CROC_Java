2e65c2f8aa91ffc7c7e36539428cba764299df57
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-26 15:58:57 +0300

    feat(Main): change package name according to the convention and remove creation of AuctionFileReader

--- src/ArturKuznetsov/lab7/task13/Main.java
+++ src/ArturKuznetsov/lab7/task13/Main.java
@@ -1,24 +1,22 @@
 package ArturKuznetsov.lab7.task13;
 
-import ArturKuznetsov.lab7.task13.Auctuion.AuctionFileReader;
-import ArturKuznetsov.lab7.task13.Auctuion.AuctionLot;
-import ArturKuznetsov.lab7.task13.Auctuion.AuctionParticipant;
+import ArturKuznetsov.lab7.task13.auction.AuctionFileReader;
+import ArturKuznetsov.lab7.task13.auction.AuctionLot;
+import ArturKuznetsov.lab7.task13.auction.AuctionParticipant;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        AuctionFileReader fileReader = new AuctionFileReader();
-
         String lotFilePath = "src/ArturKuznetsov/lab7/task13/textFiles/lot.txt";
-        ArrayList<AuctionLot> lots = fileReader.readLotFile(lotFilePath);
+        ArrayList<AuctionLot> lots = AuctionFileReader.readLotFile(lotFilePath);
         System.out.println("Лоты аукциона:");
         for (AuctionLot lot : lots) {
             System.out.println("Лот: " + lot.getName() + ", Цена: " + lot.getStartedPrice());
         }
 
         String participantsFilePath = "src/ArturKuznetsov/lab7/task13/textFiles/participants.txt";
-        ArrayList<String> participantsName = fileReader.readParticipantFile(participantsFilePath);
+        ArrayList<String> participantsName = AuctionFileReader.readParticipantFile(participantsFilePath);
         System.out.println("\nУчастники аукциона:");
         ArrayList<AuctionParticipant> participants = new ArrayList<>();
         for (String participantName : participantsName) {
